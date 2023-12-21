c01aeb4292cbb1146ef404b3c4547dacd62f1d5b
Hashif <hashif.batcha@outlook.fr>
2023-12-21 18:52:29 +0100

    modification du menu (couleur icon) et fix bug et des pages accueil, demande d'Ahdésion et Mon Profil

--- fam-tree-front/src/components/navbar/SideMenu.jsx
+++ fam-tree-front/src/components/navbar/SideMenu.jsx
@@ -6,6 +6,9 @@ import {useSelector} from "react-redux";
 
 const {Sider} = Layout;
 const {SubMenu} = Menu;
+
+const selectedIconColor = "#4CC425";
+
 const SideMenu = () => {
     const location = useLocation();
     const userData = JSON.parse(localStorage.getItem('userData'));
@@ -43,11 +46,17 @@ const SideMenu = () => {
             >
                 {userData && (
                     <>
-                        <Menu.Item key="/presentation" icon={<UserOutlined/>} style={{marginBottom: '15px'}}>
+                        <Menu.Item key="/presentation"
+                                   icon={<UserOutlined
+                                       style={{color: location.pathname === '/presentation' ? selectedIconColor : 'white'}}/>}
+                                   style={{marginBottom: '15px'}}>
                             <Link to="/presentation"
                                   style={{color: location.pathname === '/presentation' ? '#333' : 'white'}}>Présentation</Link>
                         </Menu.Item>
-                        <Menu.Item key="/family-tree" icon={<UserOutlined/>} style={{marginBottom: '15px'}}>
+                        <Menu.Item key="/family-tree"
+                                   icon={<UserOutlined
+                                       style={{color: location.pathname === '/family-tree' ? selectedIconColor : 'white'}}/>}
+                                   style={{marginBottom: '15px'}}>
                             <Link to="/family-tree"
                                   style={{color: location.pathname === '/family-tree' ? '#333' : 'white'}}>Arbre
                                 Généalogique</Link>
@@ -55,7 +64,10 @@ const SideMenu = () => {
                     </>
                 )}
 
-                {!userData && (<Menu.Item key="/home" icon={<UserOutlined/>} style={{marginBottom: '15px'}}>
+                {!userData && (<Menu.Item
+                    key="/home"
+                    icon={<UserOutlined style={{color: location.pathname === '/home' ? selectedIconColor : 'white'}}/>}
+                    style={{marginBottom: '15px'}}>
                     <Link to="/home"
                           style={{color: location.pathname === '/home' ? '#333' : 'white'}}>Accueil</Link>
                 </Menu.Item>)}
@@ -64,23 +76,23 @@ const SideMenu = () => {
                     user?.firstLogin === false
                     && (
                         <>
-                            <SubMenu key="admin" title={<span style={{color: 'white'}}
-                            >Administrateur</span>}
-                                     icon={<UserOutlined style={{color: "white"}}/>} style={{background: 'transparent'}}>
+                            <SubMenu key="admin" title={<span style={{color: 'white'}}>Administrateur</span>}
+                                     icon={<UserOutlined style={{color: 'white'}}/>}
+                                     style={{background: 'transparent'}}>
                                 <Menu.Item key="/adhesion/dashboard">
                                     <Link to="/adhesion/dashboard"
-                                          style={{color: location.pathname === '/adhesion/dashboard' ? 'black' : 'black'}}>Adhésion
+                                          style={{color: location.pathname === '/adhesion/dashboard' ? '#333' : '#333'}}>Adhésion
                                         Dashboard</Link>
                                 </Menu.Item>
                             </SubMenu>
                         </>
                     )}
                 {!userData && (
-                    <Menu.Item key="/adhesion/apply" icon={<UserOutlined/>}>
+                    <Menu.Item key="/adhesion/apply" icon={<UserOutlined
+                        style={{color: location.pathname === '/adhesion/apply' ? selectedIconColor : 'white'}}/>}>
                         <Link to="/adhesion/apply"
                               style={{color: location.pathname === '/adhesion/apply' ? '#333' : 'white'}}>Demande
-                            d'adhésion
-                        </Link>
+                            d'adhésion</Link>
                     </Menu.Item>
                 )}
 
@@ -101,18 +113,28 @@ const SideMenu = () => {
                 {
                     userData ?
                         <div className='w-full'>
-                            <Menu.Item key="/profile" icon={<UserOutlined/>}>
+                            <Menu.Item
+                                key="/profile"
+                                icon={<UserOutlined
+                                    style={{color: location.pathname === '/profile' ? selectedIconColor : 'white'}}/>}
+                                style={{
+                                    marginBottom: '15px',
+                                    color: location.pathname === '/profile' ? '#333' : 'white',
+                                    backgroundColor: location.pathname === '/profile' ? '#E6F4FF' : 'transparent',
+                                }}>
                                 <Link to="/profile"
                                       style={{color: location.pathname === '/profile' ? '#333' : 'white'}}>Mon
                                     Profil</Link>
                             </Menu.Item>
-                            <Menu.Item key="/logout" icon={<UserOutlined/>}>
+                            <Menu.Item key="/logout" icon={<UserOutlined
+                                style={{color: location.pathname === '/logout' ? selectedIconColor : 'white'}}/>}>
                                 <Link to="/logout"
                                       style={{color: location.pathname === '/logout' ? '#333' : 'white'}}>Déconnexion</Link>
                             </Menu.Item>
                         </div>
                         :
-                        <Menu.Item key="/login" icon={<UserOutlined />}>
+                        <Menu.Item key="/login" icon={<UserOutlined
+                            style={{color: location.pathname === '/login' ? selectedIconColor : 'white'}}/>}>
                             <Link to="/login"
                                   style={{color: location.pathname === '/login' ? '#333' : 'white'}}>Connexion</Link>
                         </Menu.Item>
