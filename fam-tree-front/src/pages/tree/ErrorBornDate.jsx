885355d8f8c38f747bfc0f4c20b71197164dc3f8
Shaik <armesclasseb@gmail.com>
2023-12-28 13:41:13 +0100

    quick fixs

--- fam-tree-front/src/pages/tree/ErrorBornDate.jsx
+++ fam-tree-front/src/pages/tree/ErrorBornDate.jsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { Button } from "antd";
+import {Button} from "antd";
 import ErrorBornDateButton from "../../components/button/ErrorBornDateButton";
 import '../../components/button/PowerButton.css';
 
-const ErrorBornDate = ({ onHide }) => (
+const ErrorBornDate = ({onHide}) => (
     <div className="conteneur">
-    <Result
-        status="error"
-        title="Vos données sont incohérentes"
-            subTitle="Assurez vous de résoudre les erreurs listées ci-dessous"
-        extra={[
-            <Button type="primary" key="console" className="billyButton" onClick={onHide}>
-                FTPro
-            </Button>,
-        ]}
-    >
-        <ErrorBornDateButton />
+        <ErrorBornDateButton/>
         <Button type="primary" key="console" className="billyButton okButton" onClick={onHide}>
             Ok
         </Button>
