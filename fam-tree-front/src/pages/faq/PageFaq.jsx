871dc74f75833bae13fc0df27548d465831c3534
Hashif <hashif.batcha@outlook.fr>
2024-01-01 21:21:04 +0100

    ajouts des questions & réponses FAQ

--- fam-tree-front/src/pages/faq/PageFaq.jsx
+++ fam-tree-front/src/pages/faq/PageFaq.jsx
@@ -19,11 +19,10 @@ const PageFaq = () => {
                             </div>
                             <div>
                                 <div className="flex items-center h-16">
-                                    <span className="text-lg text-green-ftpro font-bold">Lorem ipsum dolor sit amet?</span>
+                                    <span className="text-lg text-green-ftpro font-bold">Comment puis-je commencer à créer mon arbre généalogique sur Arbre Généalogique Pro++ ?</span>
                                 </div>
                                 <div className="flex items-center py-2">
-                                    <span className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!</span>
-
+                                    <span className="text-gray-500">Pour commencer, inscrivez-vous et créez un compte. Une fois connecté, vous pouvez créer un nouvel arbre en ajoutant des informations sur votre famille. Suivez les instructions faciles à comprendre pour ajouter des membres de la famille.</span>
                                 </div>
                             </div>
                         </div>
@@ -40,11 +39,10 @@ const PageFaq = () => {
                             <div>
                                 <div className="flex items-center h-16">
                                     <span
-                                        className="text-lg text-green-ftpro font-bold">Consectetur adipisicing elit?</span>
+                                        className="text-lg text-green-ftpro font-bold">Est-ce que je peux ajouter un partenaire et un enfant en même temps sur mon arbre généalogique ?</span>
                                 </div>
                                 <div className="flex items-center py-2">
-                                    <span className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!</span>
-
+                                    <span className="text-gray-500">Non, il faut d'abord ajouter un partenaire et puis l'enfant. Cela nous permet de garantir que les liens familiaux sont correctement établis et représentés dans votre arbre généalogique.</span>
                                 </div>
                             </div>
                         </div>
@@ -60,11 +58,10 @@ const PageFaq = () => {
                             </div>
                             <div>
                                 <div className="flex items-center h-16">
-                                    <span className="text-lg text-green-ftpro font-bold">Neque eos, dignissimos provident reiciendis debitis?</span>
+                                    <span className="text-lg text-green-ftpro font-bold">Est-ce que je peux discuter avec n'importe qui dans la rubrique Discussions ?</span>
                                 </div>
                                 <div className="flex items-center py-2">
-                                    <span className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!</span>
-
+                                    <span className="text-gray-500">Non, il faut que la personne soit inscrite sur le site et qu'elle soit présente sur votre arbre. Cette mesure assure que les échanges restent pertinents et sécurisés au sein de votre cercle familial étendu sur Arbre Généalogique Pro++.</span>
                                 </div>
                             </div>
                         </div>
@@ -80,11 +77,67 @@ const PageFaq = () => {
                             </div>
                             <div>
                                 <div className="flex items-center h-16">
-                                    <span className="text-lg text-green-ftpro font-bold">Repudiandae commodi perferendis et itaque?</span>
+                                    <span className="text-lg text-green-ftpro font-bold">Que faire si je rencontre des problèmes techniques sur le site ?</span>
                                 </div>
                                 <div className="flex items-center py-2">
-                                    <span className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!</span>
+                                    <span className="text-gray-500">Si vous rencontrez des problèmes techniques, veuillez contacter notre support client via l'adresse e-mail contact@ftpro.com. Nous nous efforçons de répondre rapidement pour résoudre tout problème.</span>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div className="mt-4 flex">
+                            <div>
+                                <div className="flex items-center h-16 border-l-4 border-green-ftpro">
+                                    <span className="text-4xl text-green-ftpro px-4">Q.</span>
+                                </div>
+                                <div className="flex items-center h-16 border-l-4 border-gray-400">
+                                    <span className="text-4xl text-gray-400 px-4">R.</span>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="flex items-center h-16">
+                                    <span className="text-lg text-green-ftpro font-bold">Comment puis-je partager des photos et des histoires avec un membre qui est présent sur mon arbre généalogique ?</span>
+                                </div>
+                                <div className="flex items-center py-2">
+                                    <span className="text-gray-500">Vous pouvez partager des photos et des récits avec les membres de votre arbre en utilisant la fonctionnalité de messagerie dans la rubrique Discussions. Pour ce faire, ouvrez une conversation avec la personne concernée, cliquez sur l'icône en forme de trombone pour joindre des photos et saisissez votre texte pour raconter des histoires ou partager des informations. Cela permet d'enrichir votre arbre généalogique avec des souvenirs et des anecdotes personnels.</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="mt-4 flex">
+                            <div>
+                                <div className="flex items-center h-16 border-l-4 border-green-ftpro">
+                                    <span className="text-4xl text-green-ftpro px-4">Q.</span>
+                                </div>
+                                <div className="flex items-center h-16 border-l-4 border-gray-400">
+                                    <span className="text-4xl text-gray-400 px-4">R.</span>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="flex items-center h-16">
+                                    <span className="text-lg text-green-ftpro font-bold">Est-il possible de supprimer un membre de mon arbre généalogique ?</span>
+                                </div>
+                                <div className="flex items-center py-2">
+                                    <span className="text-gray-500">Oui, vous pouvez supprimer un membre en naviguant sur son profil dans votre arbre et en sélectionnant l'option 'Supprimer'. Cela retirera la personne de votre arbre généalogique.</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="mt-4 flex">
+                            <div>
+                                <div className="flex items-center h-16 border-l-4 border-green-ftpro">
+                                    <span className="text-4xl text-green-ftpro px-4">Q.</span>
+                                </div>
+                                <div className="flex items-center h-16 border-l-4 border-gray-400">
+                                    <span className="text-4xl text-gray-400 px-4">R.</span>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="flex items-center h-16">
+                                    <span className="text-lg text-green-ftpro font-bold">Que faire si j'ai entré des informations incorrectes sur un membre de la famille ?</span>
+                                </div>
+                                <div className="flex items-center py-2">
+                                    <span className="text-gray-500">Vous pouvez modifier les informations à tout moment en cliquant sur le profil du membre concerné et en sélectionnant 'Modifier'. Vous pourrez alors corriger ou ajouter de nouvelles informations.</span>
                                 </div>
                             </div>
                         </div>
